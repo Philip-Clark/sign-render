4bf0d50a1fbe4b4bf798fbd72bacaf326b1379d9
Philip Clark <ContactPhilipClark@protonmail.com>
2023-07-11 18:11:00 -0400

    add simple sign features

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import { useState } from 'react';
 import './App.css';
 import Canvas from './Canvas';
-import * as Icons from '@mui/icons-material';
+import OptionsPanel from './optionsPanel';
 
 function App() {
+  const [selectedTemplate, setSelectedTemplate] = useState(0);
+  const [selectedWood, setSelectedWood] = useState(0);
+  const [selectedColor, setSelectedColor] = useState(0);
+  const [size, setSize] = useState('16x16');
+  const [firstName, setFirstName] = useState('John');
+  const [lastName, setLastName] = useState('Doe');
+
   return (
     <div className="App">
-      <Canvas />
-
-      <p>
-        <a href="https://www.freepik.com/free-psd/living-room-wall-psd-japandi-interior_16216036.htm#query=interior%20mockup&position=1&from_view=search&track=ais">
-          Image by rawpixel.com
-        </a>{' '}
-        on Freepik
-      </p>
+      <OptionsPanel
+        getters={{ selectedTemplate, selectedWood, selectedColor, size, firstName, lastName }}
+        setters={{
+          setSelectedTemplate,
+          setSelectedWood,
+          setSelectedColor,
+          setSize,
+          setFirstName,
+          setLastName,
+        }}
+      />
+      <Canvas
+        template={selectedTemplate}
+        wood={selectedWood}
+        color={selectedColor}
+        firstName={firstName}
+        lastName={lastName}
+        size={size}
+      />
     </div>
   );
 }
